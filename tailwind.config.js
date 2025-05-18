6d5230fe8ebec54cb535f2b7101870b31400b3b9
Alexander Cyril <sanchez.alexander.cyril@com>
2025-05-18 18:04:49 +0100

    Added src folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/lib/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     container: {
